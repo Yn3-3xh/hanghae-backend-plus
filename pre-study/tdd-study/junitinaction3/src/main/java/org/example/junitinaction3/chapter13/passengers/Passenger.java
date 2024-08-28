b2fcafbbbb8d6a341e0d68bf0bca1d06c36029e6
Yn3-3xh <tong13579@naver.com>
2024-08-29 01:20:04 +0900

    insert new fiture on chapter13

--- pre-study/tdd-study/junitinaction3/src/main/java/org/example/junitinaction3/chapter13/passengers/Passenger.java
+++ pre-study/tdd-study/junitinaction3/src/main/java/org/example/junitinaction3/chapter13/passengers/Passenger.java
@@ -1,17 +1,21 @@
 package org.example.junitinaction3.chapter13.passengers;
 
 import lombok.Getter;
-import lombok.ToString;
+import lombok.Setter;
+import org.example.junitinaction3.chapter13.flights.Flight;
 
 import java.util.Arrays;
 import java.util.Locale;
 
 @Getter
+@Setter
 public class Passenger {
 
     private String identifier;
     private String name;
     private String countryCode;
+
+    private Flight flight;
 
     public Passenger(String identifier, String name, String countryCode) {
         if (!Arrays.asList(Locale.getISOCountries()).contains(countryCode)) {
@@ -20,6 +24,21 @@ public class Passenger {
         this.identifier = identifier;
         this.name = name;
         this.countryCode = countryCode;
+    }
+
+    public void joinFlight(Flight flight) {
+        Flight previousFlight = this.flight;
+        if (null != previousFlight) {
+            if (!previousFlight.removePassenger(this)) {
+                throw new RuntimeException("승객을 삭제할 수 없습니다");
+            }
+        }
+        setFlight(flight);
+        if (null != flight) {
+            if (!flight.addPassenger(this)) {
+                throw new RuntimeException("승객을 추가할 수 없습니다");
+            }
+        }
     }
 
     @Override
